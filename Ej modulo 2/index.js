bc83f5052faca87ff343cfa4319ea35886a869b7
leamgon <leamgon@gmail.com>
2022-05-24 01:46:10 -0300

    Datos persona

---
+++ Ej modulo 2/index.js
@@ -0,0 +1,7 @@
+const datosPersonales = ["Leandro", 32, true, new Date(1989, 08, 17), {
+    titulo: "Cien años de soledad",
+    autor: "Gabriel Garcia Marquez",
+    fecha: new Date(1967, 2, 5),
+    url: "https://es.wikipedia.org/wiki/Cien_a%C3%B1os_de_soledad"
+}]
+console.log(datosPersonales);
