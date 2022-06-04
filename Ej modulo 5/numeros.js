fd24519c2eaf4063eb4cbe637e441a61ca24b8e4
leamgon <leamgon@gmail.com>
2022-06-03 21:53:09 -0300

    Ejercicios del modulo 5

---
+++ Ej modulo 5/numeros.js
@@ -0,0 +1,13 @@
+let altura = 184;
+let alturaM = 1.84;
+let peso = 86.6;
+let alturaR = Math.round(alturaM)
+console.log(alturaR);
+let pesoA = Math.floor(peso);
+console.log(pesoA);
+let maxValorJs = Number.MAX_VALUE;
+console.log(maxValorJs);
+let max = maxValorJs + 1
+console.log(max);
+let comprueba = maxValorJs === max;
+console.log(comprueba);
