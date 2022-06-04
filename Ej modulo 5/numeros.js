a1a75da436f52de18a859446ae9e10ba90af92ff
leamgon <leamgon@gmail.com>
2022-06-03 22:13:49 -0300

    Pasado a limpio

--- Ej modulo 5/numeros.js
+++ Ej modulo 5/numeros.js
@@ -2,12 +2,7 @@ let altura = 184;
 let alturaM = 1.84;
 let peso = 86.6;
 let alturaR = Math.round(alturaM)
-console.log(alturaR);
 let pesoA = Math.floor(peso);
-console.log(pesoA);
 let maxValorJs = Number.MAX_VALUE;
-console.log(maxValorJs);
 let max = maxValorJs + 1
-console.log(max);
 let comprueba = maxValorJs === max;
-console.log(comprueba);
