687f4b0a45049d1619542189c3be4f5a5e342cff
leamgon <leamgon@gmail.com>
2022-06-14 15:57:35 -0300

    Solo se linckeo con otro js

--- Ej modulo 7/fechas.js
+++ Ej modulo 7/fechas.js
@@ -3,5 +3,5 @@ console.log(fechaHoy.toLocaleDateString('en-GB'));
 const fechaNacimiento = new Date("09/17/1989");
 const esMasTarde = fechaHoy > fechaNacimiento;
 const diaNacimiento = fechaNacimiento.getDate();
-const mesNacimiento = fechaNacimiento.getMonth();
+const mesNacimiento = fechaNacimiento.getMonth() + 1;
 const añoNacimiento = fechaNacimiento.getFullYear();
