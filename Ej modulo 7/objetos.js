be29d49c14f93eb36c00d7f9b7502a9c5dfe18ec
leamgon <leamgon@gmail.com>
2022-06-14 15:51:29 -0300

    Se añadio el obejto leandro con el factor de propagacion

--- Ej modulo 7/objetos.js
+++ Ej modulo 7/objetos.js
@@ -8,9 +8,10 @@ const leandro = {
 let edad = leandro.edad;
 
 const amigos = [
-    {nombre: "Leandro", apellido: "González", edad: 32, altura: 1.76, eresDesarrollador: true},
+    { ...leandro },
     {nombre: "Brenda", apellido: "Carrizo", edad: 34, altura: 1.60, eresDesarrollador: false},
     {nombre: "Lucas", apellido: "Scevola", edad: 28, altura: 1.71, eresDesarrollador: false}
 ]
 
 const amigosOrdenados = amigos.sort((a,b)=> b.edad - a.edad);
+console.log(amigos);
